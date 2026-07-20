15737c3cd690f6d5d414e313042a064a01da45de
burakcnaksy0 <aksoyburak808@gmail.com>
2026-07-20 15:59:41 +0300

    swagger-config added

--- src/main/java/com/burakcanaksoy/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/burakcanaksoy/springsecurity/config/SecurityConfig.java
@@ -34,6 +34,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/totp/verify-login").hasRole("PRE_AUTH")
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
